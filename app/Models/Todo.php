e84dbf35351c79f176510a0d74eb1903cbce8b28
boula1997 <boula.nessim.soliman@gmail.com>
2022-06-12 00:42:54 +0200

    Created the controller and model with the CRUD methods

---
+++ app/Models/Todo.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'todos';
+    public $timestamps = true;
+
+
+
+    protected $fillable = [
+        'task',
+        'staus',
+        'due',
+    ];
+}
