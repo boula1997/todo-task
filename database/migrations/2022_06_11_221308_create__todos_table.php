24ef7c799a95f09b3db310ce29c3bec768074b2b
boula1997 <boula.nessim.soliman@gmail.com>
2022-06-12 00:17:17 +0200

    Craeted todos table using migration

---
+++ database/migrations/2022_06_11_221308_create__todos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string('task', 600)->nullable();
+            $table->string('staus', 500)->default("Incomplete");
+            $table->timestamp('due');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
